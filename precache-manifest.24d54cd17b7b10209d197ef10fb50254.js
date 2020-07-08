f3f881a5384c14aaa8164b33b814b5acf8900723
ErickDalet <isc.villanueva@hotmail.com>
2020-07-08 00:13:04 -0500

    Updates

---
+++ precache-manifest.24d54cd17b7b10209d197ef10fb50254.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b919d3bb09ac53ed724f5099797d205d",
+    "url": "/Entregable3-RactApp/index.html"
+  },
+  {
+    "revision": "8e68d2a05558abeb13b7",
+    "url": "/Entregable3-RactApp/static/css/main.bae86081.chunk.css"
+  },
+  {
+    "revision": "4cc0600669f4ee6c11e8",
+    "url": "/Entregable3-RactApp/static/js/2.8d0790f3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Entregable3-RactApp/static/js/2.8d0790f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e68d2a05558abeb13b7",
+    "url": "/Entregable3-RactApp/static/js/main.0a76b303.chunk.js"
+  },
+  {
+    "revision": "c9ec1d557955d4c77bdf",
+    "url": "/Entregable3-RactApp/static/js/runtime-main.bb6aa16e.js"
+  },
+  {
+    "revision": "ed59e2d5decab5ca1e215093f7eda995",
+    "url": "/Entregable3-RactApp/static/media/adm1.ed59e2d5.png"
+  },
+  {
+    "revision": "4cd045df18a6f2232db6edbd78505d25",
+    "url": "/Entregable3-RactApp/static/media/inicio.4cd045df.jpg"
+  }
+]);
